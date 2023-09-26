e3982fdd3ef4004f809ebff8e912e7a8ac150c48
“mikaeloling” <mikael.oling@gmail.com>
2023-09-26 15:15:01 +0300

    lightbulb true false switches color

--- src/components/Lightbulb.jsx
+++ src/components/Lightbulb.jsx
@@ -1,13 +1,30 @@
+import { useState } from "react";
 export const Lightbulb = () => {
   // Initialize the state for on/off
+  const [color, setColor] = useState(false);
 
   const toggleLight = () => {
     // Toggle the on/off state
+    
+    setColor((c) => !c);
+    console.log(color);
+    //write code here
+
     console.log("Clicked");
+    
+    // Change the color of the lightbulb
   };
+
+  color ? console.log("yellow") : console.log("black");
+
   return (
     <div className="lightbulb-container">
-      <div className="lightbulb" onClick={toggleLight}>
+      <div className= {
+        `${"lightbulb"} ${color ? "on" : "off"}
+        `} 
+        // Add the class "on" or "off" based on the state
+
+        onClick={toggleLight}>
         <div className="bulb"></div>
       </div>
     </div>
